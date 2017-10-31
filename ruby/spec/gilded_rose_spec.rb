8b125181e6a277a7464fa345d4cf1b64bb842e2f
benjaminsunderland <ben@sunderland.eu.com>
2017-10-31 13:25:30 +0000

    Added test for the item 'sulfuras' to test for quality and sell-in change

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -10,4 +10,13 @@ describe GildedRose do
       expect(items[0].name).to eq "fixme"
     end
   end
+
+  describe "#items" do
+    it "does not change the quality of Sulfuras" do
+      items = [ItemCreation.new("Sulfuras, Hand of Ragnaros", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq(0)
+      expect(items[0].quality).to eq(0)
+    end
+  end
 end
